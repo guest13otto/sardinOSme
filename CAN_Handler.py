a7d36fb7e130f4cdbf4487b5b73419768c538610
IsaacSinn <43310667+IsaacSinn@users.noreply.github.com>
2020-07-29 18:15:22 +0800

    Update CAN_Handler.py
    New:
    print log.sent & log.error

--- CAN_Handler.py
+++ CAN_Handler.py
@@ -10,12 +10,10 @@ cansend
 Publish Topics:
 
 log.sent:
-    address: Integer
-    data: Tuple
+    message
 
 log.error:
-    address: Integer
-    data: Tuple
+    message
 
 '''
 
@@ -34,22 +32,27 @@ class CAN_Handler(Module):
 
         try:
             self.bus.send(msg)
-            pub.sendMessage("log.sent" + msg)
+            pub.sendMessage("log.sent" , message = msg)
 
         except Can.CanError:
-            pub.sendMessage("log.error" + msg)
+            pub.sendMessage("log.error" , message = msg)
 
     def run(self):
         message = self.bus.recv(1)
 
 class __Test_Case_Send__(Module):
-    data = [0x20, 32767>>8 & 0xFF, 32767 & 0xFF]
-    pub.sendMessage('cansend', 0x01C, data)
+    data = [0x20, 7000>>8 & 0xFF, 7000 & 0xFF]
+    pub.sendMessage('cansend', 0x0ff, data)
 
 if __name__ == "__main__":
-    import time
+
+    def logger(message):
+        print(message)
+
     can_handler = CAN_Handler()
     can_handler.start(10)
+    pub.subscribe(logger, "log.sent")
+    pub.subscribe(logger, "log.error")
 
     test_case_send = __Test_Case_Send__()
     test_case_send.start(10)
