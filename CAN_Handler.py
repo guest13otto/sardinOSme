a9e0c83cfe6654e77fc73605cde9574cc22b5ab1
RickyVu <rickyvu189@gmail.com>
2020-07-30 18:29:49 +0800

    added CAN_Handler.py

--- CAN_Handler.py
+++ CAN_Handler.py
@@ -1,3 +1,4 @@
+
 '''
 CAN_Handler Module
 
@@ -28,11 +29,10 @@ class CAN_Handler(Module):
     def __init__(self):
         self.bus = can.interface.Bus(bustype = "socketcan", channel = "can0", bitrate = 250000)
         pub.subscribe(self.message_listener, "cansend")
+        #notifier = can.Notifier(self.bus, [CAN_Listener])
 
     def message_listener(self, data):
         msg = can.Message(arbitration_id = data[0], data = data[1:], is_extended_id = False)
-
-
         try:
             self.bus.send(msg)
             pub.sendMessage("log.sent" , message = msg)
@@ -40,32 +40,34 @@ class CAN_Handler(Module):
         except can.CanError:
             pub.sendMessage("log.error" , message = msg)
 
+
     def run(self):
-        message = self.bus.recv(1)
-        pub.sendMessage("log.receive", message = message)
+        message = self.bus.recv(0.5)
+        #print("received:", message)
+        pub.sendMessage("can.receive", message = message)
 
 class __Test_Case_Send__(Module):
     def run(self):
-        data = (0xff, 0x20, 3000>>8 & 0xFF, 3000 & 0xFF)
+        data = (0xff, 0x10, 0000>>8 & 0xFF, 0000 & 0xFF)
         pub.sendMessage('cansend', data = data)
 
 if __name__ == "__main__":
 
     def logger_sent(message):
-        print("message sent: ", message)
+        print("log.sent: ", message)
 
     def logger_error(message):
-        print("message not sent: ", message)
+        print("log.error: ", message)
 
     def logger_receive(message):
-        print("message received: ", message)
+        print("log.receive: ", message)
 
     can_handler = CAN_Handler()
     can_handler.start(10)
 
     test_case_send = __Test_Case_Send__()
-    test_case_send.start(10)
+    test_case_send.start(0.4)
 
-    pub.subscribe(logger_sent, "log.sent")
+    # pub.subscribe(logger_sent, "log.sent")
     pub.subscribe(logger_error, "log.error")
-    pub.subscribe(logger_receive, "log.receive")
+    pub.subscribe(logger_receive, "can.receive")
