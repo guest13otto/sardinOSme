b4e8c9f7da7db98cbbef07a0a8acfce33d58b225
RickyVu <rickyvu189@gmail.com>
2020-07-31 18:02:00 +0800

    update CAN_Handler.py New: changed pub format to byte array can.receive include extras, timestamp

--- CAN_Handler.py
+++ CAN_Handler.py
@@ -1,24 +1,26 @@
 
+
 '''
 CAN_Handler Module
 
 Subscribe Topics:
 
-cansend
-    data: Tuple
-    data[1]: address <hexadecimal>
-    data[2]: command <hexadecimal>
+can.send
+    address <hexadecimal>
+    data <bytearrray>
 
 Publish Topics:
 
-can.sent:
-    message
+log.sent:
+    message frame
 
-can.error:
-    message
+log.error:
+    message frame
 
 can.receive.<arbitration_id>:
-    message
+    data <bytearray>
+    extra <dictionary>
+	"timestamp" <float>
 
 '''
 
@@ -29,29 +31,31 @@ from pubsub import pub
 class CAN_Handler(Module):
     def __init__(self):
         self.bus = can.interface.Bus(bustype = "socketcan", channel = "can0", bitrate = 250000)
-        pub.subscribe(self.message_listener, "cansend")
+        pub.subscribe(self.message_listener, "can.send")
         #notifier = can.Notifier(self.bus, [CAN_Listener])
 
-    def message_listener(self, data):
-        msg = can.Message(arbitration_id = data[0], data = data[1:], is_extended_id = False)
+    def message_listener(self, address, data):
+        msg = can.Message(arbitration_id = address, data = data, is_extended_id = False)
         try:
             self.bus.send(msg)
-            pub.sendMessage("can.sent" , message = msg)
+            pub.sendMessage("log.sent" , message = msg)
 
         except can.CanError:
-            pub.sendMessage("can.error" , message = msg)
+            pub.sendMessage("log.error" , message = msg)
 
 
     def run(self):
-        message = self.bus.recv(0.5)
+        message = self.bus.recv(1)
         #print("received:", message)
         if message != None:
-            pub.sendMessage("can.receive", message = message)
+            topic_name = "can.receive." + str(hex(message.arbitration_id))[2:]
+            #print("topic_name: ", topic_name)
+            pub.sendMessage(topic_name, message = message.data, extra = {"timestamp": message.timestamp})
+            
 
 class __Test_Case_Send__(Module):
     def run(self):
-        data = (0xff, 0x10)
-        pub.sendMessage('cansend', data = data)
+        pub.sendMessage('can.send', address = 0xff,data = bytearray([0x10]))
 
 if __name__ == "__main__":
 
@@ -61,14 +65,14 @@ if __name__ == "__main__":
     def logger_error(message):
         print("log.error: ", message)
 
-    def logger_receive(message):
-        print("log.receive: ", message.arbitration_id)
+    def logger_receive(message, extra):
+        print("can.receive: ", message, "can.extra: ", extra)
 
     can_handler = CAN_Handler()
-    can_handler.start(10)
+    can_handler.start(1)
 
     test_case_send = __Test_Case_Send__()
-    test_case_send.start(0.4)
+    test_case_send.start(1)
 
     pub.subscribe(logger_sent, "log.sent")
     pub.subscribe(logger_error, "log.error")
