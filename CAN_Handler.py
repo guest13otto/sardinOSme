18db3bcfa3ad1a447fb5ccfe3744fcfca44e37ec
IsaacSinn <sinweitengisaac@gmail.com>
2020-07-31 12:39:26 +0800

    Update CAN_Handler.py
    Need to test in rpi 4

--- CAN_Handler.py
+++ CAN_Handler.py
@@ -5,18 +5,19 @@ CAN_Handler Module
 Subscribe Topics:
 
 cansend
-    address: Integer
     data: Tuple
+    data[1]: address <hexadecimal>
+    data[2]: command <hexadecimal>
 
 Publish Topics:
 
-log.sent:
+can.sent:
     message
 
-log.error:
+can.error:
     message
 
-log.receive:
+can.receive.<arbitration_id>:
     message
 
 '''
@@ -35,20 +36,21 @@ class CAN_Handler(Module):
         msg = can.Message(arbitration_id = data[0], data = data[1:], is_extended_id = False)
         try:
             self.bus.send(msg)
-            pub.sendMessage("log.sent" , message = msg)
+            pub.sendMessage("can.sent" , message = msg)
 
         except can.CanError:
-            pub.sendMessage("log.error" , message = msg)
+            pub.sendMessage("can.error" , message = msg)
 
 
     def run(self):
         message = self.bus.recv(0.5)
         #print("received:", message)
-        pub.sendMessage("can.receive", message = message)
+        if message != None:
+            pub.sendMessage("can.receive", message = message)
 
 class __Test_Case_Send__(Module):
     def run(self):
-        data = (0xff, 0x10, 0000>>8 & 0xFF, 0000 & 0xFF)
+        data = (0xff, 0x10)
         pub.sendMessage('cansend', data = data)
 
 if __name__ == "__main__":
@@ -60,7 +62,7 @@ if __name__ == "__main__":
         print("log.error: ", message)
 
     def logger_receive(message):
-        print("log.receive: ", message)
+        print("log.receive: ", message.arbitration_id)
 
     can_handler = CAN_Handler()
     can_handler.start(10)
@@ -68,6 +70,6 @@ if __name__ == "__main__":
     test_case_send = __Test_Case_Send__()
     test_case_send.start(0.4)
 
-    # pub.subscribe(logger_sent, "log.sent")
+    pub.subscribe(logger_sent, "log.sent")
     pub.subscribe(logger_error, "log.error")
     pub.subscribe(logger_receive, "can.receive")
