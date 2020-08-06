a73e8e04f851fcdca683e4051bb70d9e202f95cb
IsaacSin <43310667+IsaacSinn@users.noreply.github.com>
2020-08-06 13:45:59 +0800

    Update Thruster_Power.py
    ThrusterMatrix

--- Thruster_Power.py
+++ Thruster_Power.py
@@ -4,16 +4,27 @@ import numpy as np
 
 class Thruster_Power(Module):
     def __init__ (self, CG, ThrusterFL, ThrusterFR, ThrusterBL, ThrusterBR, ThrusterUF, ThrusterUB):
-        self.CG = CG
+        self.CG = np.array(tuple(map(float, CG.split(','))))
         self.ThrusterFL = ThrusterFL
         self.ThrusterFR = ThrusterFR
         self.ThrusterBL = ThrusterBL
         self.ThrusterBR = ThrusterBR
         self.ThrusterUF = ThrusterUF
         self.ThrusterUB = ThrusterUB
+        self.ThrusterMatrix = np.zeros((6,1))
 
-        FLposition = tuple(map(float, self.ThrusterFL["Position"].split(',')))
-        print(FLposition)
+        Thrusters = (self.ThrusterFL, self.ThrusterFR, self.ThrusterBL, self.ThrusterBR, self.ThrusterUF, self.ThrusterUB)
+
+        for Thruster in Thrusters:
+            ThrusterPosition = np.array(tuple(map(float, Thruster["Position"].split(','))))
+            ThrusterDirection = np.array(tuple(map(float, Thruster["Direction"].split(','))))
+            ThrusterPosition = np.subtract(ThrusterPosition, self.CG)
+            Torque = np.cross(ThrusterPosition, ThrusterDirection)
+            ThrusterArray = np.concatenate((ThrusterDirection, Torque)).reshape(6,1)
+            self.ThrusterMatrix = np.concatenate((self.ThrusterMatrix, ThrusterArray), axis = 1)
+        print(self.ThrusterMatrix)
+
+
 
         pub.subscribe(self.command_movement, "command.movement")
 
