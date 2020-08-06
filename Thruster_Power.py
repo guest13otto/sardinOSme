ada5223cbe8089f5f68e3901ecda7707c4451e6c
IsaacSinn <43310667+IsaacSinn@users.noreply.github.com>
2020-08-06 22:25:41 +0800

    Update Thruster_Power.py
    New:
    -combine yaml loader with thruster_power
    -added test case

--- Thruster_Power.py
+++ Thruster_Power.py
@@ -13,25 +13,29 @@ Thruster.Power
 from Module_Base import Module
 from pubsub import pub
 import numpy as np
+import yaml
 
 
 class Thruster_Power(Module):
-    def __init__ (self, CG, ThrusterFL, ThrusterFR, ThrusterBL, ThrusterBR, ThrusterUF, ThrusterUB):
+    def __init__ (self):
+        try:
+            content = yaml.load(open('Thruster.yaml', 'r'), Loader = yaml.FullLoader)
+            for key,value in content.items():
+                exec(f"self.{key} = value")
+        except FileNotFoundError:
+            pass
 
-
-        CG = np.array(tuple(map(float, CG.split(','))))
+        self.CG = np.array(tuple(map(float, self.CG.split(','))))
         self.ThrusterMatrix = np.zeros((6,1))
-        self.ThrusterFL, self.ThrusterFR, self.ThrusterBL, self.ThrusterBR, self.ThrusterUF, self.ThrusterUB = ThrusterFL, ThrusterFR, ThrusterBL, ThrusterBR, ThrusterUF, ThrusterUB
-        self.Thrusters = (ThrusterFL, ThrusterFR, ThrusterBL, ThrusterBR, ThrusterUF, ThrusterUB)
+        self.Thrusters = (self.ThrusterFL, self.ThrusterFR, self.ThrusterBL, self.ThrusterBR, self.ThrusterUF, self.ThrusterUB)
 
         for Thruster in self.Thrusters: # 6x6 Matrix
             ThrusterPosition = np.array(tuple(map(float, Thruster["Position"].split(','))))
             ThrusterDirection = np.array(tuple(map(float, Thruster["Direction"].split(','))))
-            ThrusterPosition = np.subtract(ThrusterPosition, CG)
+            ThrusterPosition = np.subtract(ThrusterPosition, self.CG)
             Torque = np.cross(ThrusterPosition, ThrusterDirection)
             ThrusterArray = np.concatenate((ThrusterDirection, Torque)).reshape(6,1)
             self.ThrusterMatrix = np.concatenate((self.ThrusterMatrix, ThrusterArray), axis = 1)
-        print(self.ThrusterMatrix[0:6, 1:7])
 
         pub.subscribe(self.command_movement, "command.movement")
 
@@ -41,13 +45,35 @@ class Thruster_Power(Module):
 
         ThrusterMatrixInv = np.linalg.pinv(self.ThrusterMatrix[0:6,1:7])
         finalList = ThrusterMatrixInv.dot(expectedResult)
-        print(finalList)
+        #print(self.ThrusterMatrix)
 
         for counter, Thruster in enumerate(self.Thrusters):
             if Thruster["Invert"] == True:
                 finalList[counter, 0] *= -1
 
+        pub.sendMessage("Thruster.Power", message = finalList)
+
 
 
     def run(self):
         pass
+class __Test_Case_Send__(Module):
+    def run(self):
+        pub.sendMessage("command.movement", message = (0,0,0,0,1,0))
+
+if __name__ == "__main__":
+    from Gamepad import Gamepad
+    def Thruster_Power_Listener(message):
+
+        print(message.reshape(1,6))
+
+    Gamepad = Gamepad()
+    Gamepad.start(100)
+
+    Thruster_Power = Thruster_Power()
+    Thruster_Power.start(100)
+
+    test_case_send = __Test_Case_Send__()
+    test_case_send.start(1)
+
+    pub.subscribe(Thruster_Power_Listener, "Thruster.Power")
