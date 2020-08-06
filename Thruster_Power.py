c1c4b3180f810c2cf01dfb4eaac5feca5aa1c6a1
IsaacSinn <43310667+IsaacSinn@users.noreply.github.com>
2020-08-06 18:51:02 +0800

    Revert 1
    Revert for Separate Thrusters_Loader.py

--- Thruster_Power.py
+++ Thruster_Power.py
@@ -1,35 +1,53 @@
+'''
+Subcribe Topics:
+
+command.movement
+    vector6: Strafe, Drive, Yaw, UpDown, TiltFB, TiltLR
+
+Publish Topics
+
+Thruster.Power
+    vector6: Strafe, Drive, UpDown, TiltFB, TiltLR, Yaw
+'''
+
 from Module_Base import Module
 from pubsub import pub
 import numpy as np
 
+
 class Thruster_Power(Module):
     def __init__ (self, CG, ThrusterFL, ThrusterFR, ThrusterBL, ThrusterBR, ThrusterUF, ThrusterUB):
-        self.CG = np.array(tuple(map(float, CG.split(','))))
-        self.ThrusterFL = ThrusterFL
-        self.ThrusterFR = ThrusterFR
-        self.ThrusterBL = ThrusterBL
-        self.ThrusterBR = ThrusterBR
-        self.ThrusterUF = ThrusterUF
-        self.ThrusterUB = ThrusterUB
+
+
+        CG = np.array(tuple(map(float, CG.split(','))))
         self.ThrusterMatrix = np.zeros((6,1))
+        self.ThrusterFL, self.ThrusterFR, self.ThrusterBL, self.ThrusterBR, self.ThrusterUF, self.ThrusterUB = ThrusterFL, ThrusterFR, ThrusterBL, ThrusterBR, ThrusterUF, ThrusterUB
+        self.Thrusters = (ThrusterFL, ThrusterFR, ThrusterBL, ThrusterBR, ThrusterUF, ThrusterUB)
 
-        Thrusters = (self.ThrusterFL, self.ThrusterFR, self.ThrusterBL, self.ThrusterBR, self.ThrusterUF, self.ThrusterUB)
-
-        for Thruster in Thrusters:
+        for Thruster in self.Thrusters: # 6x6 Matrix
             ThrusterPosition = np.array(tuple(map(float, Thruster["Position"].split(','))))
             ThrusterDirection = np.array(tuple(map(float, Thruster["Direction"].split(','))))
-            ThrusterPosition = np.subtract(ThrusterPosition, self.CG)
+            ThrusterPosition = np.subtract(ThrusterPosition, CG)
             Torque = np.cross(ThrusterPosition, ThrusterDirection)
             ThrusterArray = np.concatenate((ThrusterDirection, Torque)).reshape(6,1)
             self.ThrusterMatrix = np.concatenate((self.ThrusterMatrix, ThrusterArray), axis = 1)
-        print(self.ThrusterMatrix)
-
-
+        print(self.ThrusterMatrix[0:6, 1:7])
 
         pub.subscribe(self.command_movement, "command.movement")
 
     def command_movement(self, message):
         Strafe, Drive, Yaw, Updown, TiltFB, TiltLR = message
+        expectedResult = np.array((Strafe, Drive, Updown, TiltFB, TiltLR, Yaw)).reshape(6,1)
+
+        ThrusterMatrixInv = np.linalg.pinv(self.ThrusterMatrix[0:6,1:7])
+        finalList = ThrusterMatrixInv.dot(expectedResult)
+        print(finalList)
+
+        for counter, Thruster in enumerate(self.Thrusters):
+            if Thruster["Invert"] == True:
+                finalList[counter, 0] *= -1
+
+
 
     def run(self):
         pass
--- Thrusters_Loader.py
+++ Thrusters_Loader.py
@@ -1,5 +1,6 @@
 import yaml
 from pubsub import pub
+from Module_Base import Module
 
 class Loader():
     def load_all(self, YAML_File):
@@ -33,6 +34,7 @@ class Loader():
                 _node = varclass + "(" + args + ")"
                 print(_node)
                 _node = eval(varclass + "(" + args + ")")
+                print(f"{file} is running")
                 nodes.append({"node": _node, "class": varclass, "frequency": frequency, "args": args})
 
 
@@ -43,6 +45,10 @@ class Loader():
             #pub.sendMessage("log.ThrusterLoader.error", message = FileNotFoundError)
         return nodes
 
+class __Test_Case_Send__(Module):
+    def run(self):
+        pub.sendMessage("command.movement", message = (0,1,0,0,0,0))
+
 if __name__ == "__main__":
     #def logger_ThrusterLoader_error(message):
         #print("File not found: ", message)
@@ -51,7 +57,9 @@ if __name__ == "__main__":
     nodes = Loader.load_all("Thruster.yaml")
 
     for n in nodes:
-
         n["node"].start(n["frequency"])
+
+    test_case_send = __Test_Case_Send__()
+    test_case_send.start(1)
 
     #pub.subscribe(logger_ThrusterLoader_error, "log.ThrusterLoader.error")
