868953a5defc7708b0b4baf2cc9ed25e1d6a96f0
IsaacSinn <43310667+IsaacSinn@users.noreply.github.com>
2020-08-11 20:21:50 +0800

    Create test thruster program / thrusterControl.py

---
+++ thrusterControl.py
@@ -0,0 +1,63 @@
+
+import numpy as np
+import math
+
+#constants
+sin = math.sin(math.radians(45))
+CG = np.array((0, 0, 0))
+horizontalDist = [0.168,0.176,0] # FR x,y,z unit:m
+verticalDist = [0,0.182,0.097] # TF x,y,z unit:m
+
+FLposition = np.array((-horizontalDist[0], horizontalDist[1], horizontalDist[2]))
+FRposition = np.array((horizontalDist[0], horizontalDist[1], horizontalDist[2])) # position x,y,z
+BLposition = np.array((-horizontalDist[0], -horizontalDist[1], horizontalDist[2]))
+BRposition = np.array((horizontalDist[0], -horizontalDist[1], horizontalDist[2]))
+TLposition = np.array((verticalDist[0], verticalDist[1], verticalDist[2]))
+TRposition = np.array((verticalDist[0], -verticalDist[1], verticalDist[2]))
+
+FLthrust = np.array((sin, sin, 0))
+FRthrust = np.array((-sin, sin, 0)) # thrust direction
+BLthrust = np.array((sin, -sin, 0))
+BRthrust = np.array((-sin, -sin, 0))
+TLthrust = np.array((0, 0, 1))
+TRthrust = np.array((0, 0, 1))
+
+def thrusterPreset(absPosition, thrustDirect):
+    position = np.subtract(absPosition, CG)
+    torque = np.cross(position, thrustDirect)
+    return np.concatenate((thrustDirect, torque)).reshape(6,1)
+
+
+def movementListener(self):
+
+    FL = thrusterPreset(FLposition, FLthrust)
+    FR = thrusterPreset(FRposition, FRthrust)
+    BL = thrusterPreset(BLposition, BLthrust)
+    BR = thrusterPreset(BRposition, BRthrust)
+    TL = thrusterPreset(TLposition, TLthrust)
+    TR = thrusterPreset(TRposition, TRthrust)
+
+    thruster = [FL, FR, BL, BR, TL, TR]
+
+    T = np.concatenate((thruster), axis=1)
+
+    Tinv = np.linalg.pinv(T)
+    output = Tinv.dot(exResult)
+    return output
+
+exResult = np.array((0,2.8,0,0,0,0)).reshape(6,1) #strafe, drive, updown, tiltFR, tiltLR, yaw
+
+result = movementListener(exResult)
+
+for counter, x in enumerate(result):
+    if x < 0:
+        result[counter,0] *= 1.65
+
+print(result)
+
+max = max(abs(result))
+print(max)
+for x in result:
+    result[counter,0] = result[counter,0] / max
+
+print(result)
