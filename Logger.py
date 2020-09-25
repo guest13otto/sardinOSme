631fc21a407d84a5f590cc77d5a077c8521a5f7c
IsaacSinn <43310667+IsaacSinn@users.noreply.github.com>
2020-09-25 20:26:09 +0800

    Logger status: class for individual logger: Not ready at all
    New:
    - class for individual logger
    Need fix:
    - adding existing handler from conf

--- Logger.py
+++ Logger.py
@@ -3,6 +3,7 @@ from Module_Base_Async import Module
 import yaml
 import logging
 import logging.config
+import copy
 
 AllTopics = {"Gamepad": 'gamepad',
              "ControlProfile": 'command',
@@ -11,54 +12,56 @@ AllTopics = {"Gamepad": 'gamepad',
              "CAN_Handler": 'log'
              }
 
+class TopicLogger:
+    # console = logging.StreamHandler()
+    # Logger_file = logging.FileHandler('console.log')
+    # Logger_console = logging.StreamHandler()
+
+    def __init__(self, topic, print, log):
+        self.topic = topic
+        self.print = print
+        self.log = log
+        self.logger = logging.getLogger(self.topic)
+
+        if self.print:
+            #https://stackoverflow.com/questions/6333916/python-logging-ensure-a-handler-is-added-only-once
+            self.logger.addHandler(console)
+        elif self.log and not self.print:
+            self.logger.addHandler(Logger_file, Logger_console)
+        elif self.log and self.print:
+            self.logger.addHandler(Logger_file, console)
+        else:
+            pass
+
+        pub.subscribe(self.listener, self.topic)
+    def listener(self, message):
+        level = message.get("logLevel")
+        try:
+            exec(f"logger.{level}({message})")
+        except KeyError:
+            exec(f"logger.debug({message})")
+
 class Logger(Module):
 
-    def Factory(self, topic):
-        print("in factory")
-        def innerListener(message):
-          print("in listener")
-          level = message.get("logLevel")
-          try:
-              print(f"self.{topic}.{level}({message})")
-          except KeyError:
-              exec(f"self.{topic}.debug({message})")
-        return innerListener
-
-    def __init__(self, Print, Log, Topics):
+    def __init__(self, print, log, topics):
         super().__init__()
         with open('LoggerConfig.yaml', 'rt') as f:
             config = yaml.safe_load(f.read())
-            #print(config)
             logging.config.dictConfig(config)
-        self.Print = Print
-        self.Log = Log
-        self.Topics = tuple(map(str, Topics.split(',')))
+        
+        self.print = print
+        self.log = log
+        self.topics = tuple(map(str, topics.split(',')))
+        print(self.topics)
 
-        for topic in self.Topics:
-            exec(f"self.{topic} = logging.getLogger('{topic}')")
-            console = logging.StreamHandler()
-            Logger_file = logging.FileHandler('console.log')
-            Logger_console = logging.StreamHandler()
-            #print(topic)
+        for topic in self.topics:
 
-            if self.Print:
-                #https://stackoverflow.com/questions/6333916/python-logging-ensure-a-handler-is-added-only-once
-                exec(f"self.{topic}.addHandler(console)")
-            elif self.Log and not self.Print:
-                exec(f"self.{topic}.addHandler(Logger_file, Logger_console)")
-            elif self.Log and self.Print:
-                exec(f"self.{topic}.addHandler(Logger_file, console)")
-            else:
-                pass
-
-            exec(f"{topic}_listener = self.Factory('{topic}')")
-            exec(f'pub.subscribe({topic}_listener, "{topic}")')
-            #exec(f'pub.subscribe(innerListeners, "{topic}")')
+            exec(f"{topic} = TopicLogger({topic}, {self.print}, {self.log})")
 
     def run(self):
         pass
 
 
 if __name__ == "__main__":
-    Logger = Logger(Print = True, Log = False, Topics = "gamepad,command")
-    pub.sendMessage("gamepad.sdfs", message = {"logLevel": "warning","Ricky": "dehydrtion"})
+    Logger = Logger(print = True, log = False, topics = "gamepad, command")
+    pub.sendMessage("gamepad", message = {"logLevel": "warning","Ricky": "dehydrtion"})
