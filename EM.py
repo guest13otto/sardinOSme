4e7acf1708ce906f6d12fac91edf75cbcef93abe
IsaacSinn <rickyvu189@gmail.com>
2020-10-29 16:40:03 +0800

    Changes in RPi before 10/29/2020

--- EM.py
+++ EM.py
@@ -22,6 +22,7 @@ class EM(Module):
         pass
 
     def Listener(self, message):
+        #print(message)
         pub.sendMessage("can.send", message = {"address": eval(self.address), "data": EMLRcommand[str(list(message.keys())[0])][list(message.values())[0]]})
         #pub.sendMessage("can.send", message = {"address": eval(self.address), "data": [0x31, 0x10]})
 
